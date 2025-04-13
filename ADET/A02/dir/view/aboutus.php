84856007d3890fb70553d921fdfe03ac0ab8894f
Wito <louisito75@gmail.com>
2025-04-13 18:27:50 +0800

    ADET 02

--- ADET/A02/dir/view/aboutus.php
+++ ADET/A02/dir/view/aboutus.php
@@ -3,12 +3,12 @@
     <div class="m-4 mb-4">
 
 
-        <h1 class="mb-3 animate__animated animate__fadeInUp text-center">Welcome to BK®</h1>
-        <div class="text-center">
-            <img src="dir/img/Location1.webp" class="aboutus-img animate__fadeInUp">
+        <h1 class="mb-3   text-center">Welcome to BK®</h1>
+        <div class="text-center  ">
+            <img src="dir/img/Location1.webp" class="aboutus-img ">
         </div>
 
-        <p class="mt-4">
+        <p class="mt-4  ">
             Since 1954, Burger King has been serving flame-grilled flavor and real ingredients to customers around
             the world.
             We're more than fast-food chain -- we're a global community passionate about quality. taste, and
@@ -16,12 +16,12 @@
             your way.
         </p>
 
-        <h1 class="mb-3 mt-3 text-center">What to Expect?</h1>
-        <div class="text-center">
+        <h1 class="mb-3 mt-3 text-center  ">What to Expect?</h1>
+        <div class="text-center  ">
             <img src="dir/img/Location2.jpg" class="aboutus-img">
         </div>
 
-        <p class="mt-4">
+        <p class="mt-4  ">
             When you step into any of our Burger King restaurants, expect friendly service, a welcoming atmosphere,
             and meals made fresh to order. From our iconic Whopper to crispy fries and refreshing drinks. every
             visit
@@ -30,13 +30,13 @@
             friends, or treating the family, we've got something for everyone.
         </p>
 
-        <h1 class="mb-3 mt-3 text-center">Meet our team!
+        <h1 class="mb-3 mt-3 text-center  ">Meet our team!
         </h1>
-        <div class="text-center">
+        <div class="text-center  ">
             <img src="dir/img/Location3.jpg" class="aboutus-img" style="height: 600px;">
 
         </div>
-        <p class="mt-4">
+        <p class="mt-4  ">
             Our crew is the hear of what we do. At every Burger King, you'll meet a dedicated team committed to
             delivering
             fast, friendly and personalized service. We value teamwork, diversity, and creating a space where both
@@ -44,17 +44,17 @@
             guests feel at home.
         </p>
 
-        <h1 class="mt-4 text-center">Visit Us Today!</h1>
+        <h1 class="mt-4 text-center ">Visit Us Today!</h1>
 
-        <div class="text-center">
+        <div class="text-center animate__animated ">
             <img src="dir/img/Location4.jpeg" class="aboutus-img">
         </div>
-        <p>
+        <p class="mt-4  ">
             Find your nearest Burger King and experience the difference of real flame-grilled taste. Whether
             you're dining in, ordering to-go, or using delivery, we're ready to serve you with a smile
         </p>
 
-        <h3 class="mt-4 text-center">Because at Burger King,
+        <h3 class="mt-4 text-center  ">Because at Burger King,
             you can Always - Have it your way</h3>
 
 
